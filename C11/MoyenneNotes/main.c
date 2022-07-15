a3e52e759dfdd427c9537c28098464c31b0125c7
Anis Boubaker <anis@boubaker.ca>
2022-07-14 20:33:39 -0400

    C11

--- C11/MoyenneNotes/main.c
+++ C11/MoyenneNotes/main.c
@@ -8,15 +8,23 @@ double moyenne_notes(double* tab, int nb_notes);
 
 int main(void)
 {
+	double notes_statique[50] = { 0 };
 
 	double* notes = NULL;
 	int nb_notes = 0; 
+	int nouveau_nb_notes = 0;
 	double moyenne = 0; 
 
 	printf("Combien de notes? ");
 	scanf("%d", &nb_notes);
 
-	notes = (double*)malloc(sizeof(double) * nb_notes);
+	//notes = (double*)malloc(nb_notes * sizeof(double));
+	notes = (double*)calloc(nb_notes, sizeof(double) );
+	if (notes == NULL) //Vérification de l'allocation 
+	{
+		printf("Plus de mémoire!!\n");
+		exit(EXIT_FAILURE);
+	}
 
 	for (int i = 0; i < nb_notes; i++)
 	{
@@ -25,8 +33,33 @@ int main(void)
 	}
 
 	moyenne = moyenne_notes(notes, nb_notes);
-
 	printf("La moyenne des notes saisies: %lf\n", moyenne);
+
+
+	printf("Maintenant, combien de notes? ");
+	scanf("%d", &nouveau_nb_notes);
+
+	//Aggrandir le tableau alloué initialement avec malloc/calloc
+	double* nouvelle_adr;
+	nouvelle_adr = realloc(notes, nouveau_nb_notes * sizeof(double));
+	if (nouvelle_adr == NULL)
+	{
+		printf("Plus de mémoire! Recommencez...");
+	}
+	else
+	{
+		notes = nouvelle_adr;
+	}
+
+
+
+	//Plus besoin de l'espace mémoire alloué avec malloc, on le libère...
+	free(notes);
+
+	/*
+	Autres traitement ben importants
+	
+	*/
 
 	system("pause");
 	return EXIT_SUCCESS;
